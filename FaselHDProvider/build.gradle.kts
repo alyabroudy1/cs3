bbc5424325e92e402e6b79136f96cf6a32e79fa8
Abdullah Alyabroude <abu-omar.852@hotmail.com>
2026-01-04 01:23:46 +0100

    Add description field to providers for CloudStream compatibility

--- FaselHDProvider/build.gradle.kts
+++ FaselHDProvider/build.gradle.kts
@@ -10,4 +10,6 @@ cloudstream {
     tvTypes = listOf( "TvSeries" , "Movie" , "Anime" , "AsianDrama" )
 
     iconUrl = "https://www.google.com/s2/favicons?domain=faselhd.io&sz=%size%"
+    
+    description = "Watch Arabic movies and series from FaselHD"
 }
--- MyCimaProvider/build.gradle.kts
+++ MyCimaProvider/build.gradle.kts
@@ -10,4 +10,6 @@ cloudstream {
     tvTypes = listOf( "TvSeries" , "Movie" , "Anime" )
 
     iconUrl = "https://www.google.com/s2/favicons?domain=wecima.tube&sz=%size%"
+    
+    description = "Watch Arabic movies and series from MyCima"
 }
